74c47fa16341988efa2b70a16c53c4cd347116c9
Jakub Szuber <jszuber06@gmail.com>
2022-03-27 23:57:36 +0200

    Update main_queries.sql

--- main_queries.sql
+++ main_queries.sql
@@ -18,20 +18,25 @@ INNER JOIN species AS s ON s.species_id = h.species_id
 ORDER BY h.animal_id
 LIMIT 11;
 
---See most basic information about first ten animals (ordered by id)
+--See which are woeking days for driver whose departure animals fromdhotel
+SELECT
+	DISTINCT(planning_departure_date) AS "driver working days" 
+FROM hotel_1
+ORDER BY "driver working days";
+
+--See animals which anme starts which letter: b
+SELECT
+	name
+FROM hotel_1
+WHERE name ILIKE 'b%';
+
+--See 
 SELECT
 	animal_id
 	, name
-	CASE 
-		WHEN
-		
+	, SUBSTRING(owner_info FROM POSITION(' ' IN owner_info) FOR 12) AS "owner phone number"
+	--, owner_info AS "oowner name"
 FROM hotel_1
-
-
-
-
-
-
 
 
 
